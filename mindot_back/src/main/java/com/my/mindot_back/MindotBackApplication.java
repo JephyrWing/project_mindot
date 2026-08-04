0bbdd9fa770021b119c2df089ed7403def217fd5
JephyrWing <gad03302@gmail.com>
2026-08-04 13:44:54 +0900

    first commit

---
+++ mindot_back/src/main/java/com/my/mindot_back/MindotBackApplication.java
@@ -0,0 +1,13 @@
+package com.my.mindot_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindotBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindotBackApplication.class, args);
+	}
+
+}
