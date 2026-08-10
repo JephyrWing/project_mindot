4cbb20313c4ce942f33ec0d0f98afb25352b229c
bgy030448 <bgy030448@gmail.com>
2026-08-10 12:38:53 +0900

    feat: 배경 장식, 로고, 안내 문구 추가

--- mindot_front/src/App.jsx
+++ mindot_front/src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
 import './App.css'
+import Login from './components/Login/Login.jsx'
 
+// 애플리케이션의 최상위 화면을 구성하는 루트 컴포넌트 정의.
 function App() {
-  return (
-    <>
-    </>
-  )
+  // 로그인 화면 단독 렌더링.
+  return <Login />
 }
 
 export default App
