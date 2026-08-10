fe7f5ec6b234cb8b603e1717cb2bc7d2d4ae907a
bgy030448 <bgy030448@gmail.com>
2026-08-10 14:37:59 +0900

    feat: mainPage 생성

--- mindot_front/src/main.jsx
+++ mindot_front/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// 리액트 애플리케이션의 최상위 App 컴포넌트 렌더링.
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
